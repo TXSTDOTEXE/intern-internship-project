31b41417e0d0000fb51fa7cc6e9e7779e9f0b064
JamesDCowley <wuz7@txstate.edu>
2025-04-02 19:58:07 -0500

    fixed loot error

--- AutoBattler/src/Loot.java
+++ AutoBattler/src/Loot.java
@@ -1,26 +1,31 @@
 public class Loot {
     private int value;
     private String name;
-    private enum Type { GOLD, SILVER, COPPER };
-    
+
+    private enum Type {
+        GOLD, SILVER, COPPER
+    };
+
+    private Type type;
+
     public Loot(int value, String name, Type type) {
         this.value = value;
         this.name = name;
         this.type = type;
     }
-    
+
     public int getValue() {
         return value;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public Type getType() {
         return type;
     }
-    
+
     public String toString() {
         return name + " (" + type + ")";
     }
