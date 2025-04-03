e7987010404e82a97762318d9dfe541d4c3dcdcd
Joshua Mason <kon9@txstate.edu>
2025-04-02 19:12:42 -0500

    Added box with a background

--- AutoBattler/src/App.java
+++ AutoBattler/src/App.java
@@ -1,5 +1,7 @@
+import javax.swing.SwingUtilities;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(GameWindow::new);
     }
 }
---
+++ AutoBattler/src/GameWindow.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class GameWindow extends JFrame {
+    private BufferedImage background;
+    
+    public GameWindow() {
+        setTitle("EXE Autobattler");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        
+        try {
+            background = ImageIO.read(new File("AutoBattler/src/assests/background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        add(new GameCanvas());
+        setVisible(true);
+    }
+    
+    private class GameCanvas extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (background != null) {
+                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            }
+        }
+    }
+}
