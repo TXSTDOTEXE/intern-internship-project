34cc620a27641adc04054696cb530d2ff9adabc1
Joshua Mason <kon9@txstate.edu>
2025-04-02 19:51:31 -0500

    Bug fixes with machines

--- AutoBattler/src/GameWindow.java
+++ AutoBattler/src/GameWindow.java
@@ -16,7 +16,15 @@ public class GameWindow extends JFrame {
         setLocationRelativeTo(null);
         
         try {
-            background = ImageIO.read(new File("AutoBattler/src/assests/background.png"));
+            String os = System.getProperty("os.name").toLowerCase();
+            // Adjust file path based on the OS
+            String imagePath;
+            if (os.contains("win")) {
+                imagePath = "AutoBattler/src/assests/background.png"; // Windows path
+            } else {
+                imagePath = "AutoBattler\\src\\assests\\background.png"; // Mac path
+            }
+            background = ImageIO.read(new File(imagePath));
         } catch (IOException e) {
             e.printStackTrace();
         }
