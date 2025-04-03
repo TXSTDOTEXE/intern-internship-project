64bf3577de0fc951f25c51eab9770dbbc467f141
JamesDCowley <wuz7@txstate.edu>
2025-04-02 20:18:36 -0500

    changed fighter cooldown

--- AutoBattler/src/gameObjects/fighter.java
+++ AutoBattler/src/gameObjects/fighter.java
@@ -4,13 +4,15 @@ public class Fighter {
     int health;
     int attackStat;
     int defenseStat;
+    int maxCooldown;
     int cooldown;
 
-    public Fighter(int health, int attackStat, int defenseStat, int cooldown) {
+    public Fighter(int health, int attackStat, int defenseStat, int maxCooldown) {
         this.health = health;
         this.attackStat = attackStat;
         this.defenseStat = defenseStat;
-        this.cooldown = cooldown;
+        this.maxCooldown = maxCooldown;
+        this.cooldown = maxCooldown
     }
 
     public int tryAttack(int otherFighterHealth, int otherFighterDefenseStat) {
@@ -19,10 +21,11 @@ public class Fighter {
             if (otherFighterHealth < 0) {
                 otherFighterHealth = 0;
             }
-            cooldown = 5; // Reset cooldown after attack
-        } else {
+            cooldown = maxCooldown; // Reset cooldown after attack
+        } 
+        else {
             cooldown--;
         }
         return otherFighterHealth;
     }
-}   
+}
